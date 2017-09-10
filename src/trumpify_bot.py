1f0316e94af68db179a025ce097a9861012e222f
wngrahams <wgs11@georgetown.edu>
2017-09-10 01:33:14 -0400

    Prevented replies to self, comments already replied to

--- src/trumpify_bot.py
+++ src/trumpify_bot.py
@@ -1,7 +1,10 @@
+import os
 import praw
 import time
 
 __author__ = 'grahamstubbs'
+
+STR_SEARCH = "send their best"
 
 
 def authenticate():
@@ -11,19 +14,45 @@ def authenticate():
     return reddit
 
 
+def get_saved_comments():
+    if not os.path.isfile("../res/comments_replied_to.txt"):
+        comments_replied_to = []
+    else:
+        with open("../res/comments_replied_to.txt", "r") as file:
+            comments_replied_to = file.read()
+            comments_replied_to = comments_replied_to.split("\n")
+            comments_replied_to = filter(None, comments_replied_to)
+
+    return list(comments_replied_to)
+
+
 def main():
+    # use a dict for O(1) lookup
+    comments_replied_to = {}
+    saved_comments = get_saved_comments()
+    for i in range(0, len(saved_comments)):
+        comments_replied_to[saved_comments[i]] = saved_comments[i]
+
+    print(saved_comments)
     reddit = authenticate()
     while True:
-        run_bot(reddit)
+        run_bot(reddit, comments_replied_to)
 
 
-def run_bot(reddit):
+def run_bot(reddit, replies_dict):
     print("obtaining 25 comments")
     for comment in reddit.subreddit('test').comments(limit=25):
-        if "send their best" in comment.body:
+        if (STR_SEARCH.casefold() in comment.body.casefold()) \
+                and (comment.id not in replies_dict) \
+                and (comment.author != reddit.user.me()):  # dont reply to self or comments already replied to
             print("String with \"send their best\" found in comment {}".format(comment.id))
-            comment.reply("SAD! [Here's a pup to cheer you up!](http://imgur.com/r/aww/b7ILK3p)")
+            comment.reply("SAD! [Here's a pup to cheer you up!](http://imgur.com/r/aww/b7ILK3p) Pups send THEIR best.")
             print("Replied to comment " + comment.id)
+
+            replies_dict[comment.id] = comment.id
+
+            with open ("../res/comments_replied_to.txt", "a") as file:
+                file.write(comment.id + "\n")
 
     print("Sleeping for 10 seconds...")
     # Sleep for 10 seconds:
