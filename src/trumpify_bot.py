feecb6c1de9ea56f9760b36377d920527d8d9eee
wngrahams <wgs11@georgetown.edu>
2017-09-10 12:05:03 -0400

    Bot can now quote a users comment

--- src/trumpify_bot.py
+++ src/trumpify_bot.py
@@ -1,18 +1,23 @@
 import os
 import praw
+import requests
 import time
 
 __author__ = 'grahamstubbs'
 
-STR_SEARCH = "send their best"
-
-FINGERS_SPLAYED = u'\U0001F590'
+FINGERS_SPLAYED = u'\U0001F590'  # This emoji currently only works on mobile
 INDEX_POINTING_UP = u'\u261D'
 OK_HAND = u'\U0001F44C'
 OPEN_HANDS = u'\U0001F450'
 RAISED_FIST = u'\u270A'
 RAISED_HAND = u'\u270B'
 WAVING_HAND = u'\U0001F44B'
+
+STR_SEARCH = "!mycomment"
+COMMENT_REPLY = ("SAD! [Here's a pup to cheer you up!](http://imgur.com/r/aww/b7ILK3p)"
+                 "Pups send THEIR best. " + FINGERS_SPLAYED + " " + OK_HAND + " " + OPEN_HANDS + " " + WAVING_HAND)
+COMMENT_JOKE = "You requested a joke! Here it is:\n\n"
+JOKE = requests.get("http://api.icndb.com/jokes/random").json()['value']['joke']
 
 
 def authenticate():
@@ -52,13 +57,13 @@ def run_bot(reddit, replies_dict):
         if (STR_SEARCH.casefold() in comment.body.casefold()) \
                 and (comment.id not in replies_dict) \
                 and (comment.author != reddit.user.me()):  # dont reply to self or comments already replied to
-            print("String with \"send their best\" found in comment {}".format(comment.id))
-            # comment.reply("SAD! [Here's a pup to cheer you up!](http://imgur.com/r/aww/b7ILK3p) Pups send THEIR best.")
+            print("String with " + STR_SEARCH + " found in comment {}".format(comment.id))
+            comment.reply("Here is your comment:\n\n> " + comment.body)
             print("Replied to comment " + comment.id)
 
             replies_dict[comment.id] = comment.id
 
-            with open ("../res/comments_replied_to.txt", "a") as file:
+            with open("../res/comments_replied_to.txt", "a") as file:
                 file.write(comment.id + "\n")
 
     print("Sleeping for 10 seconds...")
