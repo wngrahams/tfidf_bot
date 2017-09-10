b55045ab1549417adcc9ad777de2ab4f032dd508
wngrahams <wgs11@georgetown.edu>
2017-09-09 22:41:27 -0400

    Added login method

--- src/trumpify_bot.py
+++ src/trumpify_bot.py
@@ -1,3 +1,12 @@
 __author__ = 'grahamstubbs'
 
 import praw
+import src.config as config #use when running in pycharm
+#import config              #use when running in terminal
+
+def bot_login():
+    praw.Reddit(username = config.username,
+                password = config.password,
+                client_id = config.client_id,
+                client_secret = config.client_secret,
+                user_agent = "trumpify_bot v0.1")
